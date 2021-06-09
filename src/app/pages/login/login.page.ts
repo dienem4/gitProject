e5e28af1977144165f6e372b20b0cf26feca0234
dienem4 <sdienembaye@gmail.com>
2021-06-09 19:52:31 +0200

    login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 
@@ -11,14 +13,22 @@ export class LoginPage implements OnInit {
  
   email : string='';
   pass : string='';
-  constructor(private auth:AuthService) { }
+  constructor(private router :Router,private auth:AuthService,private loading :LoadingController) { }
   
   ngOnInit() {}
   
-   loginForm (){
+   async loginForm (){
+   
+      const load = await this.loading.create({
+        cssClass: 'my-custom-class',
+        message: 'Please wait...',
+      });
+     //await load.present();
      console.log(this.email);
      console.log(this.pass);
-     this.auth.getProfile().subscribe(data=>{console.log(data);
+     this.router.navigate(['/home']);
+     this.auth.getProfile().subscribe(async(user)=>{console.log(user);
+     //await load.onDidDismiss();
     })
    }
 }
