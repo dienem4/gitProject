7068c81aff5468a25c55df47a37212f7de3c858c
dienem4 <sdienembaye@gmail.com>
2021-07-07 21:44:02 +0200

    Add feed

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ArticleFeed } from 'src/app/interfaces/article-feed';
+import { FeedsService } from 'src/app/services/feeds.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+    feeds : ArticleFeed[];
 
-  constructor() {}
+   pictures:string []=[
+     "https://cdn.britannica.com/82/212182-050-50D9F3CE/basketball-LeBron-James-Cleveland-Cavaliers-2018.jpg",
+     "https://www.gannett-cdn.com/presto/2021/03/03/PTAL/47e129cc-68b7-4832-ac3e-43ff653a62c9-JAMES.JPG?width=520&height=597&fit=crop&format=pjpg&auto=webp",
+     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGRh-iJ-duocnbfygEPTR0SAOcKnouXG7B3Q&usqp=CAU"
+   ]
+    
+ 
+  constructor(private feed: FeedsService) {}
+     async ngOnInit(){
+      this.feeds = await this.feed.requestByUrlTrashTalk()
+    }
+
+  
 
 }
---
+++ src/app/services/feeds.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+import { FeedsService } from './feeds.service';
+
+describe('FeedsService', () => {
+  let service: FeedsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(FeedsService);
+  });
+
+  it('should be created', () => {
+    service.requestByUrl(service.url[0])
+    expect(service).toBeTruthy();
+  });
+});
